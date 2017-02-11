2cc7462e737b75c75af0b94081e926a79dfad841
余婷 <superyuting2016@gmail.com>
2017-02-11 22:12:23 +0800

    implement current_cart function

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,19 @@
 Rails.application.routes.draw do
 
  root 'products#index'
+
  devise_for :users
+
  namespace :admin do
-  resources :products do
+ 	resources :products
+ end
+
+ resources :products do
   	member do
   	  post :add_to_cart
   	end
  end
- end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+ # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-   resources :products
+   
 end
